b694f6c8369feac8e3c1b8b21740e13902499750
Hardart <kaino@bk.ru>
2023-10-25 16:33:34 +0300

    add component Footer

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <Section color="secondary">
+    <div class="grid grid-cols-4 prose-h4:text-lg prose-h4:tracking-wider">
+      <Logo size="lg" />
+      <div class="flex">
+        <h4>Address:</h4>
+      </div>
+      <div class="flex">
+        <h4>Phones:</h4>
+      </div>
+      <div class="flex">
+        <h4>Social:</h4>
+      </div>
+    </div>
+  </Section>
+</template>
